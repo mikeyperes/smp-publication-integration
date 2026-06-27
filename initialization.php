78f82a3edf74fc214719cfa43a29ac741af80cf3
mikeyperes <michael@mike-ro-tech.com>
2026-06-27 02:42:47 -0400

    Use Hexa core snippets table renderer

--- initialization.php
+++ initialization.php
@@ -2,7 +2,7 @@
 /**
  * Legacy bootstrap for SMP Publication Integration.
  * Canonical main plugin file: smp-publication-integration.php
- * Version: 0.6.114
+ * Version: 0.6.132
  */
 
 if ( ! defined( "ABSPATH" ) ) {
